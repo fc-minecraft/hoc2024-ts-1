1270c54c9350692ffeb207dad3b786aaef22d4bf
neonerz <gerald@rewrite.media>
2024-07-12 09:37:49 -0400

    Update pxt.json, Instruments.jres, hoc2024.ts

--- hoc2024.ts
+++ hoc2024.ts
@@ -1,23 +1,33 @@
 //enums
 enum Drums {
-    Half = 0,
-    Mid = 1,
-    Double = 2
+    //% block="slow"
+    Slow,
+    //% block="medium"
+    Medium,
+    //% block="fast"
+    Fast
 }
 
 enum Keys {
-    a,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g
+    //% block="do"
+    Do,
+    //% block="re"
+    Re,
+    //% block="mi"
+    Mi,
+    //% block="fa"
+    Fa,
+    //% block="so"
+    So,
+    //% block="la"
+    La,
+    //% block="ti"
+    Ti
 }
 enum Instruments {
-    //% blockIdentity="blocks.custom" enumval=0 block="Pling"
-    //% jres alias=Pling
-    Pling,
+    //% blockIdentity="blocks.custom" enumval=0 block="Synth"
+    //% jres alias=Synth
+    Synth,
     //% blockIdentity="blocks.custom" enumval=1 block="Xylophone"
     //% jres alias=Xylophone
     Xylophone,
@@ -27,31 +37,27 @@ enum Instruments {
     //% blockIdentity="blocks.custom" enumval=3 block="Flute"
     //% jres alias=Flute
     Flute,
-    //% blockIdentity="blocks.custom" enumval=4 block="Bit"
-    //% jres alias=Bit
-    Bit,
     //% blockIdentity="blocks.custom" enumval=5 block="Bells"
     //% jres alias=Bells
     Bells,
     //% blockIdentity="blocks.custom" enumval=6 block="Bass"
     //% jres alias=Bass
-    Bass
+    Bass,
+    //% blockIdentity="blocks.custom" enumval=6 block="Piano"
+    //% jres alias=Piano
+    Piano
 }
 
 //%  block="HOC2024" weight=200 color=#0B3D91 icon="\uf186"
 namespace hoc2024 {
 
-    //% block="key %k %i1 %i2 %i3 drum %d"
-    //% i1.fieldEditor="gridpicker"
-    //% i1.fieldOptions.columns=4
-    //% i2.fieldEditor="gridpicker"
-    //% i2.fieldOptions.columns=4
-    //% i3.fieldEditor="gridpicker"
-    //% i3.fieldOptions.columns=4
-    export function testInstrument(k: Keys, i1: Instruments, i2: Instruments, i3: Instruments, d: Drums ): void {
-        switch (i1) {
-            case Instruments.Pling:
-                player.say("Pling")
+    //% block="instrument %i"
+    //% i.fieldEditor="gridpicker"
+    //% i.fieldOptions.columns=4
+    export function instrument(i: Instruments): void {
+        switch (i) {
+            case Instruments.Synth:
+                player.say("Synth")
                 break;
             case Instruments.Xylophone:
                 player.say("Xylophone")
@@ -62,45 +68,14 @@ namespace hoc2024 {
             case Instruments.Flute:
                 player.say("Flute")
                 break;
-            case Instruments.Bit:
-                player.say("Bit")
-                break;
             case Instruments.Bells:
                 player.say("Bells")
                 break;
             case Instruments.Bass:
                 player.say("Bass")
                 break;
-        }
-    }
-
-    //% block="key %k %i1 %i2"
-    //% i1.fieldEditor="gridpicker"
-    //% i1.fieldOptions.columns=4
-    //% i2.fieldEditor="gridpicker"
-    //% i2.fieldOptions.columns=4
-    export function testInstrument2(k: Keys, i1: Instruments, i2: Instruments): void {
-        switch (i1) {
-            case Instruments.Pling:
-                player.say("Pling")
-                break;
-            case Instruments.Xylophone:
-                player.say("Xylophone")
-                break;
-            case Instruments.Guitar:
-                player.say("Guitar")
-                break;
-            case Instruments.Flute:
-                player.say("Flute")
-                break;
-            case Instruments.Bit:
-                player.say("Bit")
-                break;
-            case Instruments.Bells:
-                player.say("Bells")
-                break;
             case Instruments.Bass:
-                player.say("Bass")
+                player.say("Piano")
                 break;
         }
     }
@@ -108,56 +83,42 @@ namespace hoc2024 {
     //% block="key %k"
     export function note(k: Keys): void {
         switch (k) {
-            case Keys.a:
-                player.say("a")
+            case Keys.Do:
+                player.say("do")
                 break;
-            case Keys.b:
-                player.say("b")
+            case Keys.Re:
+                player.say("re")
                 break;
-            case Keys.c:
-                player.say("c")
+            case Keys.Mi:
+                player.say("mi")
                 break;
-            case Keys.d:
-                player.say("d")
+            case Keys.Fa:
+                player.say("fa")
                 break;
-            case Keys.e:
-                player.say("e")
+            case Keys.So:
+                player.say("so")
                 break;
-            case Keys.f:
-                player.say("f")
+            case Keys.La:
+                player.say("la")
                 break;
-            case Keys.g:
-                player.say("g")
+            case Keys.Ti:
+                player.say("ti")
                 break;
 
         }
     }
 
-    //% block="instrument %i"
-    //% i.fieldEditor="gridpicker"
-    //% i.fieldOptions.columns=4
-    export function instrument(i: Instruments): void {
-        switch (i) {
-            case Instruments.Pling:
-                player.say("Pling")
+    //% block="drums %s"
+    export function drums(s: Drums): void {
+        switch (s) {
+            case Drums.Slow:
+                player.say("drums slow")
                 break;
-            case Instruments.Xylophone:
-                player.say("Xylophone")
+            case Drums.Medium:
+                player.say("drums medium")
                 break;
-            case Instruments.Guitar:
-                player.say("Guitar")
-                break;
-            case Instruments.Flute:
-                player.say("Flute")
-                break;
-            case Instruments.Bit:
-                player.say("Bit")
-                break;
-            case Instruments.Bells:
-                player.say("Bells")
-                break;
-            case Instruments.Bass:
-                player.say("Bass")
+            case Drums.Fast:
+                player.say("drums fast")
                 break;
         }
     }
