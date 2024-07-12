0a9670ae83f48e43964b2f730c3f3fe4d9c72ec3
neonerz <gerald@rewrite.media>
2024-07-12 10:41:24 -0400

    Update hoc2024.ts

--- hoc2024.ts
+++ hoc2024.ts
@@ -1,4 +1,77 @@
 //enums
+enum HeadWear{
+    //% block="glasses 1"
+    Head1,
+    //% block="glasses 2"
+    Head2,
+    //% block="sun glasses 1"
+    Head3,
+    //% block="sun glasses 2"
+    Head4,
+    //% block="cowboy hat"
+    Head5,
+    //% block="astronaut helmet"
+    Head6,
+    //% block="beard 1"
+    Head7,
+    //% block="beard 2"
+    Head8,
+    //% block="mask 1"
+    Head9,
+    //% block="mask 2"
+    Head10
+
+}
+
+enum MidWear {
+    //% block="white tee"
+    Mid1,
+    //% block="black tee"
+    Mid2,
+    //% block="sports jersey"
+    Mid3,
+    //% block="cowboy shirt"
+    Mid4,
+    //% block="astronaut top"
+    Mid5,
+    //% block="leather jacket"
+    Mid6,
+    //% block="trench coat"
+    Mid7,
+    //% block="bubble jacket"
+    Mid8,
+    //% block="tank top"
+    Mid9,
+    //% block="hawaiian shirt"
+    Mid10,
+
+
+}
+
+enum LowerWear {
+    //% block="dark jeans"
+    Lower1,
+    //% block="light jeans"
+    Lower2,
+    //% block="baggy pants"
+    Lower3,
+    //% block="black shorts"
+    Lower4,
+    //% block="cargo pants"
+    Lower5,
+    //% block="hip-hop pants"
+    Lower6,
+    //% block="cowboy jeans"
+    Lower7,
+    //% block="astronaut pants"
+    Lower8,
+    //% block="leather pants"
+    Lower9,
+    //% block="khakis"
+    Lower10
+
+
+}
 
 enum Dances {
     //% block="woah"
@@ -198,4 +271,109 @@ namespace hoc2024 {
         }
     }
 // END DANCE ACTIVITY
+
+// COSTUME ACTIVITY
+
+    //% block="upper %u middle %m lower %l"
+    export function costume(u: HeadWear, m: MidWear, l: LowerWear): void {
+        let costume_encoded
+        switch (u) {
+            case HeadWear.Head1:
+                costume_encoded = "1"
+                break;
+            case HeadWear.Head2:
+                costume_encoded = "2"
+                break;
+            case HeadWear.Head3:
+                costume_encoded = "3"
+                break;
+            case HeadWear.Head4:
+                costume_encoded = "4"
+                break;
+            case HeadWear.Head5:
+                costume_encoded = "5"
+                break;
+            case HeadWear.Head6:
+                costume_encoded = "6"
+                break;
+            case HeadWear.Head7:
+                costume_encoded = "7"
+                break;
+            case HeadWear.Head8:
+                costume_encoded = "8"
+                break;
+            case HeadWear.Head9:
+                costume_encoded = "9"
+                break;
+            case HeadWear.Head10:
+                costume_encoded = "10"
+                break;
+        }
+        switch (m) {
+            case MidWear.Mid1:
+                costume_encoded = costume_encoded.concat("1")
+                break;
+            case MidWear.Mid2:
+                costume_encoded = costume_encoded.concat("2")
+                break;
+            case MidWear.Mid3:
+                costume_encoded = costume_encoded.concat("3")
+                break;
+            case MidWear.Mid4:
+                costume_encoded = costume_encoded.concat("4")
+                break;
+            case MidWear.Mid5:
+                costume_encoded = costume_encoded.concat("5")
+                break;
+            case MidWear.Mid6:
+                costume_encoded = costume_encoded.concat("6")
+                break;
+            case MidWear.Mid7:
+                costume_encoded = costume_encoded.concat("7")
+                break;
+            case MidWear.Mid8:
+                costume_encoded = costume_encoded.concat("8")
+                break;
+            case MidWear.Mid9:
+                costume_encoded = costume_encoded.concat("9")
+                break;
+            case MidWear.Mid10:
+                costume_encoded = costume_encoded.concat("10")
+                break;
+
+        }
+        switch (l) {
+            case LowerWear.Lower1:
+                costume_encoded = costume_encoded.concat("1")
+                break;
+            case LowerWear.Lower2:
+                costume_encoded = costume_encoded.concat("2")
+                break;
+            case LowerWear.Lower3:
+                costume_encoded = costume_encoded.concat("3")
+                break;
+            case LowerWear.Lower4:
+                costume_encoded = costume_encoded.concat("4")
+                break;
+            case LowerWear.Lower5:
+                costume_encoded = costume_encoded.concat("5")
+                break;
+            case LowerWear.Lower6:
+                costume_encoded = costume_encoded.concat("6")
+                break;
+            case LowerWear.Lower7:
+                costume_encoded = costume_encoded.concat("7")
+                break;
+            case LowerWear.Lower8:
+                costume_encoded = costume_encoded.concat("8")
+                break;
+            case LowerWear.Lower9:
+                costume_encoded = costume_encoded.concat("9")
+                break;
+            case LowerWear.Lower10:
+                costume_encoded = costume_encoded.concat("10")
+                break;
+
+        }
+    }
 }
