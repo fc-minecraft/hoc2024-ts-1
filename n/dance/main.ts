fe03a5e5f236ebb022fa522d1b12d708b760ef57
neonerz <gerald@rewrite.media>
2024-09-06 19:39:33 -0400

    dance update

--- n/dance/main.ts
+++ n/dance/main.ts
@@ -36,7 +36,7 @@ enum _Dances_Actvity {
     Move4
 }
 
-enum _Dances_Actvity_ArmorStand {
+enum ArmorStand {
     //% block="1"
     One,
     //% block="2"
@@ -63,16 +63,74 @@ namespace hoc {
      * 
      * Valid parameters are:
      * Dance.Move1, Dance.Move2, Dance.Move3, Dance.Move4 all the way through Dance.Move14
-     */
-    //% block="character %n dance %d"
-    export function dance(d: Dance, n?: number): void {
+     */    
+    //% block="dance step %s"
+    export function dance(d: Dance): void {
         var score = "0"
-        if (typeof n == 'undefined') {
-            score = "0"
-        } else if (Number(n) > 0 && Number(n) < 5) {
-            score = String(n)
-        } else {
-            score = "0"
+        switch (d) {
+            case Dance.Move1:
+                score += "01"
+                break;
+            case Dance.Move2:
+                score += "02"
+                break;
+            case Dance.Move3:
+                score += "03"
+                break;
+            case Dance.Move4:
+                score += "04"
+                break;
+            case Dance.Move5:
+                score += "05"
+                break;
+            case Dance.Move6:
+                score += "06"
+                break;
+            case Dance.Move7:
+                score += "07"
+                break;
+            case Dance.Move8:
+                score += "08"
+                break;
+            case Dance.Move9:
+                score += "09"
+                break;
+            case Dance.Move10:
+                score += "10"
+                break;
+            case Dance.Move11:
+                score += "11"
+                break;
+        }
+        player.execute("scoreboard players set move dance.var " + score)
+        loops.pause(communicationsTimeout)
+    }
+
+    /**
+     * This function instructs which armor 
+     * stand should do which dance.
+     * 
+     * Usage Example: hoc.dance(ArmorStand.One, Dance.Move8)
+     * 
+     * Valid parameters are:
+     * Dance.Move1, Dance.Move2, Dance.Move3, Dance.Move4 all the way through Dance.Move14
+     */
+    //% block="armor stand %n dance %d"
+    export function armorstand_dance(a: ArmorStand, d: Dance): void {
+        var score = "0"
+        switch (a) {
+            case ArmorStand.One:
+                score = "1";
+                break;
+            case ArmorStand.Two:
+                score = "2";
+                break;
+            case ArmorStand.Three:
+                score = "3";
+                break;
+            case ArmorStand.Four:
+                score = "4";
+                break;
         }
         switch (d) {
             case Dance.Move1:
@@ -115,20 +173,20 @@ namespace hoc {
 
     //ACTIVITY
     //% block="armor stand %s dance %d"
-    export function _dance_activity(s: _Dances_Actvity_ArmorStand, d: _Dances_Actvity): void {
+    export function _dance_activity(s: ArmorStand, d: _Dances_Actvity): void {
         var score = "0"
 
         switch (s) {
-            case _Dances_Actvity_ArmorStand.One:
+            case ArmorStand.One:
                 score = "1";
                 break;
-            case _Dances_Actvity_ArmorStand.Two:
+            case ArmorStand.Two:
                 score = "2";
                 break;
-            case _Dances_Actvity_ArmorStand.Three:
+            case ArmorStand.Three:
                 score = "3";
                 break;
-            case _Dances_Actvity_ArmorStand.Four:
+            case ArmorStand.Four:
                 score = "4";
                 break;
         }
