9f73f31b6f0e0ffdf3020c3de850ea8398761333
neonerz <gerald@rewrite.media>
2024-10-03 11:51:06 -0400

    Update n/costume/main.ts

--- n/costume/main.ts
+++ n/costume/main.ts
@@ -144,7 +144,7 @@ enum LowerWear_Activity {
 }
 
 //GLOBAL VARIABLES
-const communicationsTimeout = 100;
+const communicationsTimeout = 50;
 
 //%  block="HOC2024" weight=200 color=#0B3D91 icon="\uf186"
 namespace hoc {
