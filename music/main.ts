3e98758ddfbf1fef7256e073c25c2ffdc4598010
neonerz <gerald@rewrite.media>
2024-10-14 17:22:04 -0400

    Update music/pxt.json, music/main.ts

--- music/main.ts
+++ music/main.ts
@@ -74,12 +74,6 @@ namespace hoc {
     // MUSIC ACTIVITY
     /**
      * This function allows you to add one of 8 different instruments.
-     * 
-     * Usage Example: hoc.add_instrument(Instrument.Guitar)
-     * 
-     * Valid parameters are:
-     * Instrument.Bass, Instrument.Bells, Instrument.Flute, Instrument.Guitar
-     * Instrument.Piano, Instrument.Synth, Instrument.Villager, Instrument.Xylophone
      */    
     //% block="add instrument %i"
     //% i.fieldEditor="gridpicker"
@@ -117,12 +111,6 @@ namespace hoc {
     /**
      * This function allows you to remove any instrument that's currently
      * added.
-     * 
-     * Usage Example: hoc.remove_instrument(Instrument.Guitar)
-     * 
-     * Valid parameters are:
-     * Instrument.Bass, Instrument.Bells, Instrument.Flute, Instrument.Guitar
-     * Instrument.Piano, Instrument.Synth, Instrument.Villager, Instrument.Xylophone
      */       
     //% block="remove instrument %i"
     //% i.fieldEditor="gridpicker"
@@ -195,11 +183,6 @@ namespace hoc {
 
     /**
      * This function allows you to set what note to play.
-     * 
-     * Usage Example: hoc.note(Note.Do)
-     * 
-     * Valid parameters are:
-     * Note.Do, Note.Re, Note.Mi, Note.Fa, Note.So, Note.La, Note.Ti
      */       
     //% block="note %k"
     export function note(k: Note): void {
@@ -239,11 +222,6 @@ namespace hoc {
 
     /**
      * This function allows you to start or stop a drum beat.
-     * 
-     * Usage Example: hoc.drums(Drums.Fast)
-     * 
-     * Valid parameters are:
-     * Drums.Slow, Drums.Medium, or Drums.Fast, Drums.None
      */    
     //% block="`Instrument.Drum` %s"
     export function drums(d: Drums): void {
